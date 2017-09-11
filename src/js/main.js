53dd71337391c1c484d577a2c4b60a8dc3ca8283
me <faizanh53@gmail.com>
2017-09-11 04:15:07 +0000

    Added html5-boilerplate, implemented skeleton design, attempted to add login popup

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+function showpopup()
+{
+ $("#loginform").fadeIn();
+ $("#loginform").css({"visibility":"visible","display":"block"});
+}
+
+function hidepopup()
+{
+ $("#loginform").fadeOut();
+ $("#loginform").css({"visibility":"hidden","display":"none"});
+}
