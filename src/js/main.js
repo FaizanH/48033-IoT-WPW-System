44614ff41e662f8ed7f14ebea495a85276e251b8
me <faizanh53@gmail.com>
2017-09-12 11:52:31 +0000

    Added navbar js functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/* Login windows */
 function showpopup()
 {
  $("#loginform").fadeIn();
@@ -8,4 +9,14 @@ function hidepopup()
 {
  $("#loginform").fadeOut();
  $("#loginform").css({"visibility":"hidden","display":"none"});
+}
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("Sidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("Sidenav").style.width = "0";
 }
