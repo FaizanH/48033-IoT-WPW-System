2c03c376cbf1a0b6fc5e2bd42f918486d1f3b665
Faizan <faizanh53@gmail.com>
2017-10-04 01:49:45 +1100

    Modal UI work, added admin login protocol.

--- public/js/login.js
+++ public/js/login.js
@@ -7,3 +7,37 @@ function DoLogin() {
     } else {}
   });
 }
+
+function loginAdmin () {
+	var user = document.getElementById('usr');
+	var pass = document.getElementById('pwd');
+
+	var coruser = 'admin';
+	var corpass = 'root';
+
+	if(user.value == coruser) {
+		if(pass.value == corpass) {
+      window.location.assign("../views/home.html");/*opens the target page while Id & password matches*/
+		}
+    else {
+			alert("Error: Incorrect Password or Username");/*displays error message*/
+		}
+	}
+  else {
+		alert("Error: Incorrect Password or Username");/*displays error message*/
+	}
+}
+
+// function DoAdminLogin(form) {
+//   /*the following code checkes whether the entered userid and password are matching*/
+//   if(form.id.value == "admin")
+//   {
+//     alert("yee");
+//     if (form.pass.value == "root"){
+//       window.location.assign("../views/home.html")/*opens the target page while Id & password matches*/
+//     }
+//     else {
+//       alert("Error Password or Username")/*displays error message*/
+//     }
+//   }
+// }
--- public/js/modal.js
+++ public/js/modal.js
@@ -11,7 +11,10 @@ var btn3 = document.getElementById("btnRain");
 var btn4 = document.getElementById("btnWind");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span0 = document.getElementsByClassName("close")[0];
+var span1 = document.getElementsByClassName("close")[1];
+var span2 = document.getElementsByClassName("close")[2];
+var span3 = document.getElementsByClassName("close")[3];
 
 // When the user clicks on the button, open the modal
 btn1.onclick = function() {
@@ -31,25 +34,27 @@ btn4.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal1.style.display = "none";
-	modal2.style.display = "none";
-	modal3.style.display = "none";
-	modal4.style.display = "none";
+span0.onclick = function() {
+  modal1.style.display = "none";
+}
+span1.onclick = function() {
+  modal2.style.display = "none";
+}
+span2.onclick = function() {
+  modal3.style.display = "none";
+}
+span3.onclick = function() {
+  modal4.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
-    }
-	else if (event.target == modal2) {
-        modal2.style.display = "none";
-    }
-	else if (event.target == modal3) {
-        modal3.style.display = "none";
-    }
-	else if (event.target == modal4) {
-        modal4.style.display = "none";
-    }
+  if (event.target == modal1)
+    modal1.style.display = "none";
+	else if (event.target == modal2)
+    modal2.style.display = "none";
+	else if (event.target == modal3)
+    modal3.style.display = "none";
+	else if (event.target == modal4)
+    modal4.style.display = "none";
 }
