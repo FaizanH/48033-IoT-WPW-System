e4786a25930e4ab56056ac6480affa007d910953
Faizan <faizanh53@gmail.com>
2017-10-05 02:10:13 +1100

    Added weather plugin for RSS functions.

--- public/js/home.js
+++ public/js/home.js
@@ -17,3 +17,29 @@ function closeNav(x) {
     document.getElementById("Sidenav").style.width = "0";
     x.classList.toggle("change");
 }
+// Docs at http://simpleweatherjs.com
+$(document).ready(function() {
+  $.simpleWeather({
+    location: 'Sydney, Australia',
+    woeid: '',
+    unit: 'c',
+    success: function(weather) {
+      tempHtml = '<p>'+weather.temp+'&deg;'+weather.units.temp+'</p>';
+      visHtml = '<p>Visibility: '+weather.visibility+'</p>';
+      rainHtml = '<p>'+weather.currently+'</p>';
+      windHtml = '<p>Chill: '+weather.wind.chill+'</br>'+'Speed: '+weather.wind.speed+'</br>'+'Direction: '+weather.wind.direction+'</p>';
+      locHtml = '<p>'+weather.city+', '+weather.region+'</p>';
+      $("#weather").html(tempHtml);
+      $("#vis").html(visHtml);
+      $("#rain").html(rainHtml);
+      $("#wind").html(windHtml);
+      $("#loc").html(locHtml);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+      $("#vis").html('<p>'+error+'</p>');
+      $("#rain").html('<p>'+error+'</p>');
+      $("#wind").html('<p>'+error+'</p>');
+    }
+  });
+});
